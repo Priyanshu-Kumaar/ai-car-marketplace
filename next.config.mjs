1dad50c436129090633000779d1087b2ebe11543
Priyanshu-Kumaar <priyanshukumar082004@gmail.com>
2025-04-15 16:42:56 +0530

    Add Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      serverComponentsHmrCache: false, // defaults to true
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "vadvrmbqqzjctapowfmq.supabase.co",
+        },
+      ],
+    },
+  
+    async headers() {
+      return [
+        {
+          source: "/embed",
+          headers: [
+            {
+              key: "Content-Security-Policy",
+              value: "frame-src 'self' https://roadsidecoder.created.app;",
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
