b465878fd15f13776125bf8c96c5f11d521bdd35
Priyanshu-Kumaar <priyanshukumar082004@gmail.com>
2025-04-15 15:00:24 +0530

    Auth Components

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SignUp } from '@clerk/nextjs'
+
+const SignUpPage = () => {
+  return (
+    <div className='pt-12 pb-7'> <SignUp/></div>
+  )
+}
+
+export default SignUpPage
